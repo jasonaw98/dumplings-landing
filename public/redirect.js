9bdc54591ef85af8cfafdae0fca62847d409b35f
Jason Aw <jason_aw1998@hotmail.com>
2026-03-04 17:21:55 +0800

    patch again

--- public/redirect.js
+++ public/redirect.js
@@ -1,6 +1,6 @@
 (function() {
     // Check for your specific problematic hash pattern
-    if (window.location.hash === '#how?utm_source=packaging&utm_medium=qr') {
+    if (window.location.hash === '#how?utm_source=packaging&utm_medium=qr' || window.location.hash === '#how/?utm_source=packaging&utm_medium=qr') {
       // Construct the correct URL
       var baseUrl = window.location.origin + window.location.pathname;
       var newUrl = baseUrl + '?utm_source=packaging&utm_medium=qr#how';
